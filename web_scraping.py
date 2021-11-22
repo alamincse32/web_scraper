81e5c8c5d2684100ca87954e471fe438e885a301
alamincse32 <alamincse32@gmail.com>
2021-11-22 10:18:18 -0600

    sepated comments from regular user from admin

--- web_scraping.py
+++ web_scraping.py
@@ -5,14 +5,16 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
+import html
 
-post_and_comment_text = []
+post_and_comment_text_regular_user = []
+post_and_comment_text_admin_user = []
 column_data = []
 forum_data = []
 BASE_URL = 'https://www.nationaleatingdisorders.org'
 HEADER = {'User-Agent': 'Mozilla/5.0'}
-START_DATE = datetime.strptime('02/29/2020','%m/%d/%Y')
-END_DATE = datetime.strptime('07/01/2018','%m/%d/%Y')
+START_DATE = datetime.strptime('02/29/2020', '%m/%d/%Y')
+END_DATE = datetime.strptime('01/01/2018', '%m/%d/%Y')
 
 
 # get_request = Request(BASE_URL,headers=header)
@@ -47,7 +49,9 @@ END_DATE = datetime.strptime('07/01/2018','%m/%d/%Y')
 # print(data_frame)
 
 def scrape_data(post_link):
-    post_data_comment = []
+    post_data = []
+    comment_data_for_regular_user = []
+    comment_data_for_admin_user = []
     post_text = ""
     url = BASE_URL + post_link
     get_post_request = Request(url, headers=HEADER)
@@ -55,46 +59,61 @@ def scrape_data(post_link):
 
     post_page = BeautifulSoup(post_page, 'lxml')
     # post_number = post_link.split('/')
-    post_container = post_page.find('div', {'about':  post_link})#'post-' + post_number[len(post_number) - 1]})
+    post_container = post_page.find('div', {'about': post_link})  # 'post-' + post_number[len(post_number) - 1]})
 
     if post_container is not None:
         post_date = post_container.find('div', {'class': 'forum-posted-on'})
         date = str(post_date.text).split(" ")[1]
-        post_title = str(post_container.find('div', {'class': 'forum-post-title'}).text).replace(" ", "").replace("\n",
-                                                                                                               "")
-        print(datetime.strptime(date,'%m/%d/%Y'))
-        if END_DATE <= datetime.strptime(date,'%m/%d/%Y') <= START_DATE:
-            post_data_comment.append(date)
-            if len(post_title) > 0:
-                post_data_comment.append(post_title)
-            post_author = post_container.find('div',{'class' : 'author-pane'})
-            author_name = post_author.find('span',{'class' : 'username'}).text
+
+        # check date before and after pandemic
+        if END_DATE <= datetime.strptime(date, '%m/%d/%Y') <= START_DATE:
+            # Save Author Name
+            post_author = post_container.find('div', {'class': 'author-pane'})
+            author_name = post_author.find('span', {'class': 'username'}).text
             if len(author_name) > 0:
-                post_data_comment.append(author_name)
+                post_data.append(author_name)
+
+            # save the date when author posted on NEAD
+            post_data.append(date)
+
+            # get the post title and save which show the relevance with post content
+            post_title = str(post_container.find('div', {'class': 'forum-post-title'}).text).replace("  ", "").replace(
+                "\n", "")
+            if len(post_title) > 0:
+                post_data.append(post_title)
+            # get the post content and save it
             posts = post_container.find('div', {'class': 'forum-post-content'}).findAll('p')
-
             for post in posts:
-                post_text = post_text + str(post.text)
-
+                post_text = post_text + html.unescape(str(post.text).replace('\\\'', '\''))
             if len(post_text) > 0:
-                post_data_comment.append(post_text)
+                post_data.append(post_text)
 
+            # Now get the comment section of this post.
             comment_container = post_page.find('div', {'id': 'forum-comments'})
             if comment_container is not None:
-                comments_date_panel = comment_container.findAll('div', {'class':'forum-post-info clearfix'})
+                comments_date_panel = comment_container.findAll('div', {'class': 'forum-post-info clearfix'})
                 comments_body_panel = comment_container.findAll('div', {'class': 'forum-post-wrapper'})
 
                 for i in range(len(comments_date_panel)):
-                    date_panel = comments_date_panel[i].find('div',{'class' : 'forum-posted-on'})
-                    date = str(date_panel.text).replace("\n","").replace(" ","")
-                    user_name_spane = comments_body_panel[i].find('span',{'class' : 'username'})
+                    date_panel = comments_date_panel[i].find('div', {'class': 'forum-posted-on'})
+                    date = str(date_panel.text).replace("\n", "").replace(" ", "")
+                    title_panel = comments_body_panel[i].find('div', {'class': 'forum-post-title'})
+                    comment_title = str(title_panel.string).replace("\n","").replace("  ","")
+                    user_name_spane = comments_body_panel[i].find('span', {'class': 'username'})
                     user_name = str(user_name_spane.text)
                     comment_data = ""
                     for p in comments_body_panel[i].findAll('p'):
-                        comment_data += str(p.text)
-                    post_data_comment.append(user_name)
-                    post_data_comment.append(date)
-                    post_data_comment.append(comment_data)
+                        comment_data += html.unescape(str(p.text)).replace('\\\'', '\'')
+                    if user_name == '_admin_moderator':
+                        comment_data_for_admin_user.append(user_name)
+                        comment_data_for_admin_user.append(date)
+                        comment_data_for_admin_user.append(comment_title)
+                        comment_data_for_admin_user.append(comment_data)
+                    else:
+                        comment_data_for_regular_user.append(user_name)
+                        comment_data_for_regular_user.append(date)
+                        comment_data_for_regular_user.append(comment_title)
+                        comment_data_for_regular_user.append(comment_data)
                 # print(len(comments_date_panel) == len(comments_body_panel))
                 # for comment in comment_container.findAll('div', {'class': ['forum-post-info clearfix','forum-post-wrapper']}):
                 #     comment_date_panel = comment_container.find('div',{'class' : 'forum-posted-on'})
@@ -113,8 +132,9 @@ def scrape_data(post_link):
             # else:
             #     print("The date limit has been crossed")
             #     print(len(post_data_comment))
-        print(post_data_comment)
-    return post_data_comment
+
+    # print(post_data)
+    return [post_data, comment_data_for_regular_user, comment_data_for_admin_user]
 
 
 def next_page(link):
@@ -126,9 +146,10 @@ def next_page(link):
     for row in table_body.findAll('tr'):
         post_link = row.find('a')['href']
         data = scrape_data(post_link)
-        if len(data) > 0:
-            post_and_comment_text.append(data)
-        else:
+        if len(data[0]) > 0 and len(data[1]):
+            post_and_comment_text_regular_user.append(data[0]+data[1])
+        if len(data[0]) > 0 and len(data[2]) > 0:
+            post_and_comment_text_admin_user.append(data[0]+data[2])
             # print(post_link)
             ret_variable = False
             # break
@@ -159,18 +180,21 @@ def load_forum(forum_link):
         for row in table_body.findAll('tr'):
             post_link = row.find('a')['href']
             data = scrape_data(post_link)
-            if len(data) > 0:
-                post_and_comment_text.append(data)
+            if len(data[0]) > 0 and len(data[1]) > 0:
+                post_and_comment_text_regular_user.append(data[0]+data[1])
+            if len(data[0]) > 0 and len(data[2]) > 0:
+                post_and_comment_text_admin_user.append(data[0]+data[2])
         load_forum_next_page(link)
-        if len(post_and_comment_text) > 0:
-            data_frame = pd.DataFrame(post_and_comment_text)
-            file_name = str(forum_link).split("/")
-            file_name = file_name[len(file_name) - 1]
-            print(file_name)
-            data_frame.to_csv("Before_Pendamic/" + file_name + ".csv")
-            post_and_comment_text.clear()
-        else:
-            print("There is no data in this forum. Thanks for trying to scrape data from this site\n")
+        file_name = str(forum_link).split("/")
+        file_name = file_name[len(file_name) - 1]
+        if len(post_and_comment_text_regular_user) > 0:
+            data_frame = pd.DataFrame(post_and_comment_text_regular_user)
+            data_frame.to_csv("Before_Pendamic_Regular_User/" + file_name + ".csv", encoding='utf-8-sig')
+            post_and_comment_text_regular_user.clear()
+        if len(post_and_comment_text_admin_user) > 0:
+            data_frame = pd.DataFrame(post_and_comment_text_admin_user)
+            data_frame.to_csv("Before_Pendamic_Admin_User/" + file_name + ".csv", encoding='utf-8-sig')
+            post_and_comment_text_admin_user.clear()
 
 
 def create_url_for_next_page(url):
@@ -188,12 +212,12 @@ def create_url_for_next_page(url):
         forum_post_number = str(row.find('td', {"class": "forum-number-posts"}).text).replace(" ", "").replace("\n", "")
         forum_data.append([forum_name, forum_link, forum_topic_number, forum_post_number])
         load_forum(forum_link)
+
     forum_data_frame = pd.DataFrame(forum_data,
                                     columns=['Forum Name', 'Forum Link', 'Forum Topic Number', 'Forum Post Numer'])
     forum_data_frame.to_csv('forum_data_frame.csv')
 
 
 if __name__ == '__main__':
-    # scrape_data('/forum/35214')
+    # scrape_data('/forum/38299')
     create_url_for_next_page(BASE_URL + "/forum")
-
